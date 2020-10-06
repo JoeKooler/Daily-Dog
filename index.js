af543f637c8b5d8b1a61fb268fe58a0d75224801
JoeKooler <jojesin@gmail.com>
2020-10-06 11:44:40 +0700

    Request => Axios

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const request = require("request");
+const axios = require("axios");
 const app = express();
 const port = process.env.PORT || 4000;
 app.use(express.json());
@@ -11,12 +11,14 @@ app.post("/webhook", (req, res) => {
 });
 
 app.listen(port);
-const reply = (reply_token) => {
+
+const reply = (reply_token, msg) => {
   let headers = {
     "Content-Type": "application/json",
     Authorization:
       "Bearer {/D8XrAW3FkMhUhCjzg0DmverCN4M8ZnrgC+ZiMWw6Ib18WKyWM0TUlJ0XZVFiAs1a3bPXtXYmQWpexwgAy0LyqteuV51GvAQYZ+nE0Jo9b7BpQAc1LX67tWHGao0PEf6WmOsmXAgRDWTrGIsggEaiQdB04t89/1O/w1cDnyilFU=    }",
   };
+
   let body = JSON.stringify({
     replyToken: reply_token,
     messages: [
@@ -30,14 +32,23 @@ const reply = (reply_token) => {
       },
     ],
   });
-  request.post(
-    {
-      url: "https://api.line.me/v2/bot/message/reply",
-      headers: headers,
-      body: body,
-    },
-    (err, res, body) => {
-      console.log("status = " + res.statusCode);
-    }
-  );
+  if (msg === "RandomDog") {
+    let body = JSON.stringify({
+      replyToken: reply_token,
+      messages: [
+        {
+          type: "image",
+          text: "Hello",
+        },
+        {
+          type: "text",
+          text: "How are you?",
+        },
+      ],
+    });
+  }
+
+  axios.post("https://api.line.me/v2/bot/message/reply", body, {
+    headers: headers,
+  });
 };
